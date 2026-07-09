8493fee580233debc223919d4e2158e05b6235dd
erengazimutlu <mutluerengazi@proton.me>
2026-07-09 09:19:10 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,29 +1,78 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"log"
+	"os"
 
 	"gator/internal/config"
 )
 
+type state struct {
+	cfg *config.Config
+}
+
+type command struct {
+	name string
+	args []string
+}
+
+type commands struct {
+	handlers map[string]func(*state, command) error
+}
+
+func (c *commands) register(name string, f func(*state, command) error) {
+	c.handlers[name] = f
+}
+
+func (c *commands) run(s *state, cmd command) error {
+	handler, ok := c.handlers[cmd.name]
+	if !ok {
+		return fmt.Errorf("unknown command: %s", cmd.name)
+	}
+	return handler(s, cmd)
+}
+
+func handlerLogin(s *state, cmd command) error {
+	if len(cmd.args) == 0 {
+		return errors.New("username is required")
+	}
+	username := cmd.args[0]
+
+	if err := s.cfg.SetUser(username); err != nil {
+		return err
+	}
+
+	fmt.Printf("user set to %s\n", username)
+	return nil
+}
+
 func main() {
-	// 1. Read the config file from disk
 	cfg, err := config.Read()
 	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("Config before:", cfg)
-
-	// 2. Set the current user and write the updated config to disk
-	if err := cfg.SetUser("ero"); err != nil {
-		log.Fatal(err)
+		fmt.Fprintln(os.Stderr, "error reading config:", err)
+		os.Exit(1)
 	}
 
-	// 3. Read the config file again to confirm the change was saved
-	cfg, err = config.Read()
-	if err != nil {
-		log.Fatal(err)
+	s := &state{cfg: &cfg}
+
+	cmds := &commands{handlers: make(map[string]func(*state, command) error)}
+	cmds.register("login", handlerLogin)
+
+	if len(os.Args) < 2 {
+		fmt.Fprintln(os.Stderr, "usage: gator <command> [args...]")
+		os.Exit(1)
 	}
-	fmt.Println("Config after:", cfg)
+
+	cmd := command{
+		name: os.Args[1],
+		args: os.Args[2:],
+	}
+
+	if err := cmds.run(s, cmd); err != nil {
+		if !errors.Is(err, nil) {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+	}
 }
